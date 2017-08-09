a7e548a08a54a54d1e2c2b198fc50ce492796676
Luiz R. A. Coelho <luizracoelho@gmail.com>
2017-08-09 08:48:43 -0300

    Deixar Server provider assíncrono.

--- Um416.API/Authorization/ServerProvider.cs
+++ Um416.API/Authorization/ServerProvider.cs
@@ -12,41 +12,47 @@ namespace Um416.API.Authorization
 
         public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
         {
-            context.Validated();
+            await Task.Run(() =>
+            {
+                context.Validated();
 
-            tipo = context.Parameters.FirstOrDefault(x => x.Key == "tipo").Value.FirstOrDefault();
+                tipo = context.Parameters.FirstOrDefault(x => x.Key == "tipo").Value.FirstOrDefault();
+            });
         }
 
         public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
-            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+            await Task.Run(() =>
+            {
+                context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
 
-            Usuario usuario;
+                Usuario usuario;
 
-            if (tipo == "admin")
-            {
-                var bo = new UsuarioAdminLogic();
-                usuario = bo.GetAuthenticatedUser(context.UserName, context.Password);
-            }
-            else if (tipo == "cliente")
-            {
-                var bo = new UsuarioClienteLogic();
-                usuario = bo.GetAuthenticatedUser(context.UserName, context.Password);
-            }
-            else
-            {
-                usuario = null;
-            }
+                if (tipo == "admin")
+                {
+                    var bo = new UsuarioAdminLogic();
+                    usuario = bo.GetAuthenticatedUser(context.UserName, context.Password);
+                }
+                else if (tipo == "cliente")
+                {
+                    var bo = new UsuarioClienteLogic();
+                    usuario = bo.GetAuthenticatedUser(context.UserName, context.Password);
+                }
+                else
+                {
+                    usuario = null;
+                }
 
-            if (usuario == null)
-                context.Rejected();
-            else
-            {
-                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
-                identity.AddClaim(new Claim(ClaimTypes.Name, usuario.Login));
-                identity.AddClaim(new Claim(ClaimTypes.Role, tipo));
-                context.Validated(identity);
-            }
+                if (usuario == null)
+                    context.Rejected();
+                else
+                {
+                    var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                    identity.AddClaim(new Claim(ClaimTypes.Name, usuario.Login));
+                    identity.AddClaim(new Claim(ClaimTypes.Role, tipo));
+                    context.Validated(identity);
+                }
+            });
         }
     }
 }
