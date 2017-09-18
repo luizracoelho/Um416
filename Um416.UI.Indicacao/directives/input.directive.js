e5e0e22f60ed1e0c6469ac979d0a8305ac70fb01
Luiz R. A. Coelho <luizracoelho@gmail.com>
2017-09-18 17:18:00 -0300

    Criação parcial da tela de finalização da venda.

---
+++ Um416.UI.Indicacao/directives/input.directive.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('ngApp')
+        .directive('input', input);
+
+    function input($timeout) {
+        return {
+            restrict: 'E',
+            link: function ($scope, $element, $attrs) {
+                $element.bind('keydown', function ($event) {
+                    $timeout(function () {
+                        $event.target.selectionStart = $event.target.value.length;
+                        $event.target.selectionEnd = $event.target.value.length;
+                    });
+                });
+            }
+        };
+    }
+})();
